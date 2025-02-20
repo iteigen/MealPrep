728557a3aeb7134e8cb862380995696df74b5c4a
Ian Teigen <itiegen26@students.polytechnic.org>
2025-02-20 11:41:47 -0800

    commit

--- Student.java
+++ Student.java
@@ -1,20 +1,47 @@
-public class Student{
-    private int iD;
-    private double balance;
-    private String name;
-    private static int cID=1;
-    private List<Transactions> transactions;
+import java.util.*;
 
+public class Student {
+    //fields
+    private String name;
+    private int id;
+    private double balance;
+    private static int idGen = 1;   //look at Counter class as an example
+    private ArrayList<Transaction> transactions;
+
+    //constructor
     public Student(String myName){
         name = myName;
         balance = 0;
-        iD=cID;
-        cID++;
+        id = idGen;
+        idGen++;
+        transactions = new ArrayList<Transaction>();
     }
 
-    public void Transaction(int month, int day, double amount){
+    //all the fun methods...
+    public void chargeLunch(int month, int day){
+        balance -= 7;   //hard-code for lunch amount
+        Transaction t = new Transaction(month, day, -7);
+        transactions.add(t);
+    }
 
+    //return null if there are no transactions on that day
+    public ArrayList<Transaction> getTransactionsByDate(int month, int day){
+        ArrayList<Transaction> givenDay = new ArrayList<Transaction>();
+
+        for(Transaction t : transactions){
+            if(month == t.getMonth() && day == t.getDay()){
+                givenDay.add(t);
+            }
+        }
+        if(givenDay.size() == 0){
+            return null;
+        } else {
+            return givenDay;
+        }
     }
 
 
+    public String toString(){
+        return "Name: " + name + " ID: " + id + " Balance: " + balance + " Transactions: " + transactions.toString();
+    }
 }
--- Transaction.java
+++ Transaction.java
@@ -1,6 +1,4 @@
-package MealPrep;
-
-public class Transaction {
+public class Transaction{
     private int month;
     private int day;
     private double amount;
@@ -14,17 +12,18 @@ public class Transaction {
     public int getMonth(){
         return month;
     }
+
     public int getDay(){
         return day;
     }
+
     public double getAmount(){
         return amount;
     }
-    public String toString(){
-        return "("+month + "/"+day+", $" + amount + ")";
-    }
 
-    
+    public String toString(){
+        return month + "/" + day + ": $" + amount;
+    }
 }
 
 // functions
